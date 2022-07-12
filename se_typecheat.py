890bad02d8248e02b8a7613f8c4c760f606a0d11
Chance Vionis <cvionis03@gmail.com>
2022-07-12 11:23:46 -0400

    Include necessary code for selenium to access typeracer.com Include path of chromedriver in file, set up driver, and give file access to typeracer website

--- se_typecheat.py
+++ se_typecheat.py
@@ -4,4 +4,7 @@ from selenium.webdriver.common.by import By
 import os
 import time
 
+driverPath = os.path.abspath('chromedriver')
+driver = webdriver.Chrome(driverPath)
+driver.get('https://play.typeracer.com/')
 
