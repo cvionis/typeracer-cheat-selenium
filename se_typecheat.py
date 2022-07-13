fabb3613cecbb6ce7f42a435c7f59d3cfdf358b6
Chance Vionis <cvionis03@gmail.com>
2022-07-13 13:59:01 -0400

    Move while loop into try/except statement In order to avoid exception messages upon closure of browser window Add comments for clarification of str.replace() usage

--- se_typecheat.py
+++ se_typecheat.py
@@ -14,7 +14,9 @@ raceButton = driver.find_element(By.LINK_TEXT, 'Enter a Typing Race')
 raceButton.click()
 
 promptText = (driver.find_element(By.CLASS_NAME, 'inputPanel')).text
-print(promptText)
+
+# Remove extra string following prompt text ('change display format')
+print(promptText.replace('change display format', ''))
 
 ptWords = promptText.split()
 
@@ -22,13 +24,16 @@ textInput = driver.find_element(By.CLASS_NAME, 'txtInput')
 
 gameStatus = driver.find_element(By.CLASS_NAME, 'gameStatusLabel')
 
-while True:
-    if gameStatus.text == 'The race is on! Type the text below:':
-        for word in ptWords:
-            textInput.send_keys(word + ' ')
-            time.sleep(0.5)
-    elif gameStatus.text == 'The race has ended.' or 'You finished' in gameStatus.text:
-        break
+try:
+    while True:
+        if gameStatus.text == 'The race is on! Type the text below:':
+            for word in ptWords:
+                textInput.send_keys(word + ' ')
+                time.sleep(0.25)
+        if gameStatus.text == 'The race has ended.' or 'You finished' in gameStatus.text:
+            break
+except:
+    driver.quit()
         
     
 
